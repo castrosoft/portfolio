d75b9533615148a784ccb3b8e1118f3de48d76b3
Pedro Castro <castro.fcefyn@gmail.com>
2022-04-30 22:26:45 -0300

    Termino de borrar datos irrelevantes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,11 +9,8 @@ export class AboutComponent implements OnInit {
 
   // personalInformation2: string = "10 Apr 1991";
   personalInformation: any[] = [{
-    "birthday": "10 Apr 1991",
     "website": "https://castrosoft.github.io/portfolio/",
-    "phone": "+59 3544 445189",
     "city": "Cordoba, ARG",
-    "age": "30",
     "degree": "University",
     "phEmailone": "cp.castropedro@gmail.com",
     "freelance": "Available"
