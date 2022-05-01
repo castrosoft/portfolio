b7ce7c6dcdb2f494a46005fda82edbb652f42644
Pedro Castro <castro.fcefyn@gmail.com>
2022-05-01 17:42:37 -0300

    Elimino components inutilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
-import { FactsComponent } from './facts/facts.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ResumeComponent } from './resume/resume.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ServicesComponent } from './services/services.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HeaderComponent,
     AboutComponent,
-    FactsComponent,
     SkillsComponent,
     ResumeComponent,
-    PortfolioComponent,
-    ServicesComponent,
-    TestimonialsComponent,
     ContactsComponent,
-    HeroComponent,
     FooterComponent
   ],
   imports: [
